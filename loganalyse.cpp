26719c823c542b17bccb4a91402f9fd5a94e5680
robinroyer <robin.royer@insa-lyon.fr>
2014-12-14 01:00:10 +0100

    avancement de log analyse 75%

--- loganalyse.cpp
+++ loganalyse.cpp
@@ -35,7 +35,7 @@ void loganalyse::read(bool x, bool t, int hour)
 // Algorithme :
 {
     // on ouvre le fichier en lecture
-    ifstream fichier("test.txt", ios::in);
+    ifstream fichier("anonyme.log", ios::in);
     // on cree la structure de sotckage
     struct_log data;
 
@@ -51,12 +51,43 @@ void loganalyse::read(bool x, bool t, int hour)
             data.ipEmettor=contenu.substr(0,contenu.find_first_of('[',0));
             contenu=contenu.substr(contenu.find_first_of('[',0),contenu.length());
             //data.date=
-            contenu=contenu.substr(contenu.find_first_of(']',0),contenu.length());
+            data.day=contenu.substr(0,contenu.find_first_of(' ',0));
+            contenu=contenu.substr(contenu.find_first_of(' ',0),contenu.length());
+            data.hour=contenu.substr(1,contenu.find_first_of(' ',0));
+            contenu=contenu.substr(contenu.find_first_of(' ',1),contenu.length());
+            data.gmt=contenu.substr(0,contenu.find_first_of(']',0));
+            contenu=contenu.substr(contenu.find_first_of('"',0),contenu.length());
+
             data.action=contenu.substr(0,contenu.find_first_of(' ',0));
-            contenu=contenu.substr(contenu.find_first_of('[',0),contenu.find_first_of(' ',0));
+            contenu=contenu.substr(contenu.find_first_of(' ',0),contenu.find_first_of(' ',0));
             //data.urlHit=contenu;
+            data.urlHit=contenu.substr(0,contenu.find_first_of('"',0));
+            contenu=contenu.substr(contenu.find_first_of(' ',0),contenu.length());
 
+            data.return=contenu.substr(0,contenu.find_first_of(' ',1));
+            contenu=contenu.substr(contenu.find_first_of(' ',0),contenu.length());
 
+            data.octetQuantity=contenu.substr(0,contenu.find_first_of(' ',1));
+            contenu=contenu.substr(contenu.find_first_of('"',1),contenu.length());
+
+            data.referer=contenu.substr(0,contenu.find_first_of('"',1));
+            contenu=contenu.substr(contenu.find_first_of('"',1),contenu.length());
+
+            data.browser=contenu.substr(0,contenu.find_first_of('"',1));
+
+        //afichage pour debug
+
+        cout<<data.ipEmettor<<endl;    
+        cout<<data.day<<endl;
+        cout<<data.hour<<endl;
+        cout<<data.hour<<endl;
+        cout<<data.gmt<<endl;
+        cout<<data.action<<endl;
+        cout<<data.urlHit<<endl;
+        cout<<data.return<<endl;    
+        cout<<data.octetQuantity<<endl; 
+        cout<<data.referer<<endl;               
+        cout<<data.browser<<endl;     
             // appel avec les option pour ranger des nles maps ou pas
 
             //on ferme le fichierx
@@ -114,4 +145,17 @@ loganalyse::~loganalyse ( )
 //----------------------------------------------------- Méthodes protégées
 
 //------------------------------------------------------- Méthodes privées
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+   loganalyse::read(true,true,12);
+
+
+    return 0;
+}
+
+
 
